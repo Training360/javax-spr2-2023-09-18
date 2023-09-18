bd62b9ad9e529fba3f73d1dac0b74fc4437527e9
Viczián István <viczian.istvan@gmail.com>
2023-09-18 11:22:22 +0200

    Spring Boot 3 végső projekt

--- empapp-sb3/src/main/java/empapp/NotFoundException.java
+++ empapp-sb3/src/main/java/empapp/NotFoundException.java
@@ -1,10 +1,5 @@
 package empapp;
 
-import org.zalando.problem.AbstractThrowableProblem;
-import org.zalando.problem.Status;
-
-import java.net.URI;
-
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
