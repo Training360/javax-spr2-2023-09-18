604ea1fe50c997452bed5bc6081f6de85b1d8dbc
Viczián István <viczian.istvan@gmail.com>
2023-09-18 13:46:56 +0200

    Ütemezett feladat

--- empapp-async/src/main/java/empapp/LongRunningProcessService.java
+++ empapp-async/src/main/java/empapp/LongRunningProcessService.java
@@ -1,12 +1,18 @@
 package empapp;
 
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class LongRunningProcessService {
 
     @SneakyThrows
@@ -15,5 +21,10 @@ public class LongRunningProcessService {
         log.info("Starting long process: {}", Thread.currentThread().getName());
         Thread.sleep(5_000);
         log.info("Long process end");
+    }
+
+    @Scheduled(fixedRate = 5, initialDelay = 2, timeUnit = TimeUnit.SECONDS)
+    public void tick() {
+        log.info("Tick");
     }
 }
