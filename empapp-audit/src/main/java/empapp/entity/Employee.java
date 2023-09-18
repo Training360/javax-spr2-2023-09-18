7252482f582209750ff096c8138694ab749da346
Viczián István <viczian.istvan@gmail.com>
2023-09-18 11:43:40 +0200

    Audit kiindulási projekt

---
+++ empapp-audit/src/main/java/empapp/entity/Employee.java
@@ -0,0 +1,46 @@
+package empapp.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Entity
+@Data
+@NoArgsConstructor(access = PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Address> addresses = new ArrayList<>();
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public void addAddress(Address address) {
+        addresses.add(address);
+        address.setEmployee(this);
+    }
+
+    public void addAddresses(List<Address> addresses) {
+        addresses.forEach(this::addAddress);
+    }
+}
