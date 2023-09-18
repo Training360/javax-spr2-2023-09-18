6ea89497aac72f24bf7a50491be881bb5a6e7c12
Viczián István <viczian.istvan@gmail.com>
2023-09-18 14:49:26 +0200

    Cache HashMappel

--- empapp-cache/src/main/java/empapp/EmployeeService.java
+++ empapp-cache/src/main/java/empapp/EmployeeService.java
@@ -5,6 +5,10 @@ import empapp.dto.EmployeeDto;
 import empapp.dto.UpdateEmployeeCommand;
 import empapp.entity.Employee;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
@@ -17,22 +21,29 @@ public class EmployeeService {
 
     private EmployeeMapper employeeMapper;
 
+    @CachePut(value = "employee", key = "#result.id")
+    @CacheEvict(value = "employee_list", allEntries = true)
     public EmployeeDto createEmployee(CreateEmployeeCommand command) {
         Employee employee = employeeMapper.toEmployee(command);
         employeeRepository.save(employee);
         return employeeMapper.toEmployeeDto(employee);
     }
 
+    @Cacheable("employee_list")
     public List<EmployeeDto> listEmployees() {
         return employeeMapper.toEmployeesDto(employeeRepository.findAllWithAddresses());
     }
 
+    @Cacheable("employee")
     public EmployeeDto findEmployeeById(long id) {
         return employeeMapper.toEmployeeDto(employeeRepository.findByIdWithAddresses(id)
                         .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id)));
     }
 
     @Transactional
+//    @CacheEvict(value = "employee", key = "#id")
+    @CachePut(value = "employee", key = "#id")
+    @CacheEvict(value = "employee_list", allEntries = true)
     public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
         Employee employeeToModify = employeeRepository
                 .findById(id)
@@ -41,6 +52,9 @@ public class EmployeeService {
         return employeeMapper.toEmployeeDto(employeeToModify);
     }
 
+    @Caching(evict = {
+        @CacheEvict(value = "employee", key = "#id"),
+        @CacheEvict(value = "employee_list", allEntries = true)})
     public void deleteEmployee(long id) {
         Employee employee = employeeRepository.findByIdWithAddresses(id)
                 .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id));
