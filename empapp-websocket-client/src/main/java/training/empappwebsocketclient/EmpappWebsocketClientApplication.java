2673d9a6c0793fc6c1b31e656ea8876967e38291
Viczián István <viczian.istvan@gmail.com>
2023-09-19 14:42:30 +0200

    WebSocket client topic feliratkozás

--- empapp-websocket-client/src/main/java/training/empappwebsocketclient/EmpappWebsocketClientApplication.java
+++ empapp-websocket-client/src/main/java/training/empappwebsocketclient/EmpappWebsocketClientApplication.java
@@ -1,13 +1,64 @@
 package training.empappwebsocketclient;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
-public class EmpappWebsocketClientApplication {
+@Slf4j
+public class EmpappWebsocketClientApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpappWebsocketClientApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var client = new WebSocketStompClient(new SockJsClient(List.of(new WebSocketTransport(new StandardWebSocketClient()))));
+		client.setMessageConverter(new MappingJackson2MessageConverter());
+		client.connectAsync("ws://localhost:8080/websocket-endpoint", new StompSessionHandlerAdapter() {
+			@Override
+			public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+				session.subscribe("/topic/employees", this);
+			}
+
+			@Override
+			public Type getPayloadType(StompHeaders headers) {
+				return MessageResponse.class;
+			}
+
+			@Override
+			public void handleFrame(StompHeaders headers, Object payload) {
+				if (payload instanceof MessageResponse response) {
+					log.info("Message has come: {}", response.getResponseText());
+				}
+				else  {
+					throw new IllegalStateException("Invalid class: %s".formatted(payload.getClass().getName()));
+				}
+			}
+
+			@Override
+			public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+				log.error("exception", exception);
+			}
+
+			@Override
+			public void handleTransportError(StompSession session, Throwable exception) {
+				log.error("transport", exception);
+			}
+		});
+		var scanner = new Scanner(System.in);
+		scanner.nextLine();
+	}
 }
---
+++ empapp-websocket-client/src/main/java/training/empappwebsocketclient/MessageResponse.java
@@ -0,0 +1,10 @@
+package training.empappwebsocketclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+
+    private String responseText;
+}
